a723ad8da557042cfe5cb262f19e1c9a39235f83
Ranjan sethy <sethy.ranjan@gmail.com>
2022-04-23 19:23:11 +0530

    student class created

---
+++ Oopsdemo/src/ranjan/studentmanagement/com/Student.java
@@ -0,0 +1,36 @@
+package ranjan.studentmanagement.com;
+//this is the class is to keep student details
+public class Student {
+	private int id;
+	private String name;
+	private int grade;
+	private int fees_paid;
+	private int fees_total;
+//initialize the parameters by using constructor
+	public Student(int id,String name,int grade){
+		this.id=id;
+		this.name=name;
+		this.grade=grade;
+		this.fees_total=30000;
+		this.fees_paid=0;
+		
+	}
+	/**
+	 * we need to upgrade the grades bcoz the student will promote to the next grade each year
+	 * @param grade
+	 */
+	public void setGrade(int grade){
+		this.grade=grade;
+	}
+	/**
+	 * fees need to be paid installment way or fully
+	 * @param fees
+	 */
+	public void updateFees(int fees){
+		fees_paid+=fees;
+	}
+	
+	
+
+
+}
